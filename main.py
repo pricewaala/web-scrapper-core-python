823a2bf5eb2a5af1680d77aa0d211d055620f232
pricewaala <waalaprice@gmail.com>
2023-04-10 17:38:42 +0530

    Added Diffrent Versions

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import asyncio
+import concurrent
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+import grequests
 import redis
 import requests
 from bs4 import BeautifulSoup
@@ -160,6 +166,222 @@ async def search_amazon_products(search_query: str, page: int = 1, page_size: in
     return products
 
 
+@app.get("/v3/amazon/{search_query}")
+async def search_amazon_products(search_query: str, page: int = 1, page_size: int = 30):
+    products = []
+    links_list = []
+    updated_list = []
+    start_index = (page - 1) * page_size
+    url = f"https://www.amazon.in/s?k={search_query}&page={page}"
+    print(start_index)
+    while not links_list:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "lxml")
+        links = soup.find_all("a", class_="a-link-normal s-no-outline")
+        links_list = [link.get("href") for link in links]
+
+    for link in links_list[start_index:start_index + page_size]:
+        updated_list.append(link)
+
+    # Define an async helper function to fetch the product data from the Amazon product page
+    async def fetch_product_data(link):
+        url = f"https://www.amazon.in{link}"
+        while True:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "lxml")
+            all_product_section = soup.find("div", id="dp-container")
+            if all_product_section:
+                center_product_section = all_product_section.find("div", class_="centerColAlign")
+                right_product_section = all_product_section.find("div", id="rightCol")
+                left_product_section = all_product_section.find("div", id="leftCol")
+                name = await getAmazonProductTitleName(center_product_section)
+                price = await getAmazonProductPrice(center_product_section)
+                rating_star = await getAmazonProductRatingStar(center_product_section)
+                rating_count = await getAmazonProductRatingCount(center_product_section)
+                description = await getAmazonProductDescription(center_product_section)
+                exchange_offer = await getAmazonProductExchangeAmount(right_product_section)
+                image = left_product_section.find("ul",
+                                                  class_="a-unordered-list a-nostyle a-button-list a-vertical a-spacing-top-extra-large regularAltImageViewLayout")
+                images = [n.get('src') for li in image.findAll("span", class_="a-button-inner") for n in
+                          li.find_all('img') if n.get('src') is not None] if image else []
+                product = Product(name=name, description=description, ratingStar=rating_star,
+                                  ratingCount=rating_count, price=price, exchange=exchange_offer, image=images,
+                                  link=link)
+                products.append(product)
+                print(product)
+                break
+
+    # Create a thread pool and submit the fetch_product_data coroutine for each link in updated_list
+    with ThreadPoolExecutor(max_workers=23) as executor:
+        futures = [executor.submit(asyncio.run, fetch_product_data(link)) for link in updated_list]
+        for future in futures:
+            future.result()
+
+    return products
+
+@app.get("/v4/amazon/{search_query}")
+async def search_amazon_products(search_query: str, page: int = 1, page_size: int = 30):
+    products = []
+    links_list = []
+    updated_list = []
+    start_index = (page - 1) * page_size
+    url = f"https://www.amazon.in/s?k={search_query}&page={page}"
+    print(start_index)
+    while not links_list:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "lxml")
+        links = soup.find_all("a", class_="a-link-normal s-no-outline")
+        links_list = [link.get("href") for link in links]
+
+    for link in links_list[start_index:start_index + page_size]:
+        updated_list.append(link)
+
+    # Define an async helper function to fetch the product data from the Amazon product page
+    async def fetch_product_data(link):
+        url = f"https://www.amazon.in{link}"
+        while True:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "lxml")
+            all_product_section = soup.find("div", id="dp-container")
+            if all_product_section:
+                center_product_section = all_product_section.find("div", class_="centerColAlign")
+                right_product_section = all_product_section.find("div", id="rightCol")
+                left_product_section = all_product_section.find("div", id="leftCol")
+                name = await getAmazonProductTitleName(center_product_section)
+                price = await getAmazonProductPrice(center_product_section)
+                rating_star = await getAmazonProductRatingStar(center_product_section)
+                rating_count = await getAmazonProductRatingCount(center_product_section)
+                description = await getAmazonProductDescription(center_product_section)
+                exchange_offer = await getAmazonProductExchangeAmount(right_product_section)
+                image = left_product_section.find("ul",
+                                                  class_="a-unordered-list a-nostyle a-button-list a-vertical a-spacing-top-extra-large regularAltImageViewLayout")
+                images = [n.get('src') for li in image.findAll("span", class_="a-button-inner") for n in
+                          li.find_all('img') if n.get('src') is not None] if image else []
+                product = Product(name=name, description=description, ratingStar=rating_star,
+                                  ratingCount=rating_count, price=price, exchange=exchange_offer, image=images,
+                                  link=link)
+                products.append(product)
+                print(product)
+                break
+
+    # Determine the number of links to be processed
+    num_links = len(updated_list)
+
+    # Calculate the optimal number of workers based on the number of links to be processed
+    num_workers = min(num_links, 23)
+
+    # Create a thread pool and submit the fetch_product_data coroutine for each link in updated_list
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        futures = [executor.submit(asyncio.run, fetch_product_data(link)) for link in updated_list]
+        for future in futures:
+            future.result()
+
+    return products
+
+@app.get("/v5/amazon/{search_query}")
+async def search_amazon_products(search_query: str, page: int = 1, page_size: int = 30):
+    products = []
+    links_list = []
+    updated_list = []
+    start_index = (page - 1) * page_size
+    url = f"https://www.amazon.in/s?k={search_query}&page={page}"
+    print(start_index)
+    while not links_list:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "lxml")
+        links = soup.find_all("a", class_="a-link-normal s-no-outline")
+        links_list = [link.get("href") for link in links]
+
+    for link in links_list:
+        updated_list.append(link)
+
+    links_list = []
+
+    url1 = f"https://www.amazon.in/s?k={search_query}&page=2"
+    while not links_list:
+        response = requests.get(url1)
+        soup = BeautifulSoup(response.text, "lxml")
+        links = soup.find_all("a", class_="a-link-normal s-no-outline")
+        links_list = [link.get("href") for link in links]
+
+    for link in links_list:
+        updated_list.append(link)
+
+    # Define an async helper function to fetch the product data from the Amazon product page
+    async def fetch_product_data(link):
+        url = f"https://www.amazon.in{link}"
+        while True:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "lxml")
+            all_product_section = soup.find("div", id="dp-container")
+            if all_product_section:
+                center_product_section = all_product_section.find("div", class_="centerColAlign")
+                right_product_section = all_product_section.find("div", id="rightCol")
+                left_product_section = all_product_section.find("div", id="leftCol")
+                name = await getAmazonProductTitleName(center_product_section)
+                price = await getAmazonProductPrice(center_product_section)
+                rating_star = await getAmazonProductRatingStar(center_product_section)
+                rating_count = await getAmazonProductRatingCount(center_product_section)
+                description = await getAmazonProductDescription(center_product_section)
+                exchange_offer = await getAmazonProductExchangeAmount(right_product_section)
+                image = left_product_section.find("ul",
+                                                  class_="a-unordered-list a-nostyle a-button-list a-vertical a-spacing-top-extra-large regularAltImageViewLayout")
+                images = [n.get('src') for li in image.findAll("span", class_="a-button-inner") for n in
+                          li.find_all('img') if n.get('src') is not None] if image else []
+                product = Product(name=name, description=description, ratingStar=rating_star,
+                                  ratingCount=rating_count, price=price, exchange=exchange_offer, image=images,
+                                  link=link)
+                products.append(product)
+                print(product)
+                break
+
+        # Determine the number of links to be processed
+    num_links = len(updated_list)
+
+        # Calculate the optimal number of workers based on the number of links to be processed
+    num_workers = min(num_links, 23)
+
+
+    # Create a thread pool and submit the fetch_product_data coroutine for each link in updated_list
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        futures = [executor.submit(asyncio.run, fetch_product_data(link)) for link in updated_list]
+        for future in futures:
+            future.result()
+
+    return products
+
+def callLinkAmazonV4(process_response, updated_list):
+    requests_list = [
+        grequests.get(f"https://www.amazon.in{link}", hooks={'response': process_response}, kwargs={'link': link}) for
+        link in updated_list]
+    return requests_list
+
+
+async def fetch_product_data(session, link):
+    # Fetch the product page
+    r = await session.get(f"https://www.amazon.in{link}")
+    await r.html.arender()
+
+    # Extract product data from the rendered page
+    center_product_section = r.html.find("#centerCol")[0]
+    right_product_section = r.html.find("#rightCol")[0]
+    left_product_section = r.html.find("#leftCol")[0]
+
+    name = center_product_section.find("#productTitle", first=True).text.strip()
+    price = center_product_section.find("#priceblock_ourprice, #priceblock_dealprice", first=True).text.strip()
+    rating_star = center_product_section.find("span.a-icon-alt", first=True).text.strip().split()[0]
+    rating_count = center_product_section.find("#acrCustomerReviewText", first=True).text.strip().split()[0]
+    description = center_product_section.find("#productDescription", first=True).text.strip()
+    exchange_offer = right_product_section.find(".a-box-group.a-spacing-base", first=True).text.strip()
+    images = [img.attrs["src"] for img in left_product_section.find("#altImages img")]
+
+    product = Product(name=name, description=description, ratingStar=rating_star,
+                      ratingCount=rating_count, price=price, exchange=exchange_offer, image=images,
+                      link=link)
+
+    return product
+
+
 # async def update_redis_cache(redis_url, products):
 #     redis = await aioredis.create_redis_pool(redis_url)
 #     for product in products:
@@ -205,6 +427,7 @@ async def update_redis_cache(redis_url, products):
 
         # Set the product data as a string value in Redis
         redis_client.set(product.link, str(product_data))
+
 
 @app.post("/v2/amazon/{search_query}/update_cache")
 async def update_amazon_cache(search_query: str,
